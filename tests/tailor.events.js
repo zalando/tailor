1a4cc1f6d138e156be627c673c813b79fa2052d0
Andrey Kuzmin <andrey.kuzmin@zalando.de>
2015-12-07 19:07:52 +0100

    Tests for fragments events

---
+++ tests/tailor.events.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('assert');
+const http = require('http');
+const nock = require('nock');
+const sinon = require('sinon');
+const Tailor = require('../index');
+
+describe('Tailor events', () => {
+
+    it('proxies scoped events from fragments');
+
+    it('emits `start` event');
+
+    it('emits `template:error` event');
+
+    it('emits `context:error` event');
+
+    it('emits `primary:error` event');
+
+    it('emits `primary:timeout` event');
+
+});
