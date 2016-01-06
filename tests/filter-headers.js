0723b0ae582ece94a29a15cfdaeb24f5f709712f
Andrey Kuzmin <andrey.kuzmin@zalando.de>
2016-01-06 13:28:09 +0100

    Added test file

---
+++ tests/filter-headers.js
@@ -0,0 +1,24 @@
+'use strict';
+const filterHeaders = require('../lib/filter-headers');
+const assert = require('assert');
+
+
+describe('filter-headers', () => {
+
+    const headers = {
+        'please-kill-me': '0',
+        'accept-language': '1',
+        'referer': '2',
+        'user-agent': '3'
+    };
+
+    it('keeps only certain headers', () => {
+        const after = {'accept-language': '1', 'referer': '2', 'user-agent': '3'};
+        assert.deepEqual(filterHeaders({}, headers), after);
+    });
+
+    it('removes headers if fragment is public', () => {
+        assert.deepEqual(filterHeaders({public: true}, headers), {});
+    });
+
+});
