32065c92c9a99da3fdacd0bbaaa252f5be0241d3
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2016-08-26 11:48:35 +0200

    introduce unnamed slot behaviour

--- tests/tailor.js
+++ tests/tailor.js
@@ -531,7 +531,7 @@ describe('Tailor', () => {
         });
     });
 
-    it('should support partial templates using slots', (done) => {
+    it('should support base templates using slots', (done) => {
         mockTemplate
             .returns(
                 '<head>' +
@@ -595,6 +595,42 @@ describe('Tailor', () => {
                     '<body>' +
                     '<script src=""></script>' +
                     '<h2>Last</h2>' +
+                    '</body>' +
+                    '</html>'
+                );
+                done();
+            });
+        });
+    });
+
+    it('should insert default slots if unnamed slot is present in parent template', (done) => {
+        mockTemplate
+            .returns(
+                '<head>' +
+                '</head>' +
+                '<body>' +
+                '<slot></slot>' +
+                '<h2>blah</h2>' +
+                '</body>'
+            );
+
+        mockChildTemplate
+            .returns(
+                '<script src=""></script>'
+            );
+
+        http.get('http://localhost:8080/test', (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                assert.equal(data,
+                    '<html>' +
+                    '<head></head>' +
+                    '<body>' +
+                    '<script src=""></script>' +
+                    '<h2>blah</h2>' +
                     '</body>' +
                     '</html>'
                 );
