66fd5a61ad5203f406d44ce40edc06cd2366d0e6
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2017-03-29 11:27:59 +0200

    upgrade deps and lock the deps versions

--- examples/basic-css-and-js/index.js
+++ examples/basic-css-and-js/index.js
@@ -10,6 +10,10 @@ const tailor = new Tailor({
 
 // Root Server
 http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' } );
+        return res.end('');
+    }
     tailor.requestHandler(req, res);
 }).listen(8080, function () {
     console.log('Tailor server listening on port 8080');
