de6dbe492926c1bd86c97beeb3dab854c70c6717
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-06-08 22:06:23 +0200

    #158 Add in-memory fragment templates  for fetch template.js tests

--- tests/fetch-template.js
+++ tests/fetch-template.js
@@ -2,36 +2,55 @@
 const fetchTemplate = require('../lib/fetch-template');
 const assert = require('assert');
 const sinon = require('sinon');
+const path = require('path');
+const fs = require('fs');
 
 describe('fetch-template', () => {
     const mockParseTemplate = sinon.spy();
     let mockRequest;
+    const templatePath = path.join(__dirname);
+    const testTemplatePath = `${templatePath}/test.html`;
+    const baseTemplatePath = `${templatePath}/base-template.html`;
+
+    beforeEach(() => {
+        fs.writeFileSync(testTemplatePath, '<div>test</div>');
+        fs.writeFileSync(baseTemplatePath, '<div>base-template</div>');
+    });
+
+    afterEach(() => {
+        if (fs.existsSync(testTemplatePath)) {
+            fs.unlinkSync(testTemplatePath);
+        }
+
+        if (fs.existsSync(baseTemplatePath)) {
+            fs.unlinkSync(baseTemplatePath);
+        }
+    });
 
     it('should be able to fetch the static template with absolute path', () => {
         mockRequest = {
             url: 'http://localhost:8080/test'
         };
 
-        fetchTemplate('mock-templates')(mockRequest, mockParseTemplate)
+        return fetchTemplate(templatePath)(mockRequest, mockParseTemplate)
             .then(() => {
                 assert(mockParseTemplate.calledOnce);
-                return assert(mockParseTemplate.calledWith(
-                    Buffer.from('<div>test</div>')
-                ));
+                return assert(mockParseTemplate.calledWith('<div>test</div>'));
             });
     });
 
     it('should be able to fetch template with relative path and baseTemplateFn', () => {
         mockRequest = {
-            url: 'http://localhost:8080/base-template'
+            url: 'http://localhost:8080/test'
         };
         const baseTemplateFn = () => 'base-template';
 
-        fetchTemplate('mock-templates', baseTemplateFn)(mockRequest, mockParseTemplate)
+        return fetchTemplate(templatePath, baseTemplateFn)(mockRequest, mockParseTemplate)
             .then(() => {
-                assert(mockParseTemplate.calledOnce);
+                assert(mockParseTemplate.called);
                 return assert(mockParseTemplate.calledWith(
-                    Buffer.from('<div>base-template</div>')
+                    '<div>base-template</div>',
+                    '<div>test</div>'
                 ));
             });
     });
