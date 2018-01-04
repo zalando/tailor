40cd566e8c5a6ae15ff1c4aee2697082d40a88f3
Ruben Barilani <ruben.barilani@zalando.de>
2018-01-04 16:52:42 +0100

    allow passing custom fragment attributes
    * don't exclude custom attributes when initializing Fragment instance
    * add Fragment test
    * update readme

---
+++ tests/fragment.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Fragment = require('../lib/fragment');
+const assert = require('assert');
+const getOptions = tag => {
+    return {
+        tag,
+        context: {},
+        index: false,
+        requestFragment: () => {}
+    };
+};
+
+describe('Fragment', () => {
+    it('computed and custom attributes are correctly initiliazed', () => {
+        const attributes = {
+            id: 'foo',
+            src: 'https://fragment',
+            'fallback-src': 'https://fragment-fallback',
+            async: true,
+            timeout: '4000',
+            custom: 'bar'
+        };
+
+        const expected = {
+            id: attributes.id,
+            url: attributes.src,
+            'fallback-src': attributes['fallback-src'],
+            src: attributes.src,
+            fallbackUrl: attributes['fallback-src'],
+            async: attributes.async,
+            timeout: 4000,
+            primary: false,
+            public: false,
+            custom: attributes.custom
+        };
+
+        const tag = { attributes };
+        const fragment = new Fragment(getOptions(tag));
+        const fattributes = fragment.attributes;
+
+        assert.deepEqual(fattributes, expected);
+    });
+});
