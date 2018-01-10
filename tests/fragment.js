2a2d51927d36c8237dc65bc0cf5ee024eae09b4b
Damián García <damian.garcia@zalando.de>
2018-01-10 13:00:30 +0100

    Allow setting default values for fragments attributes (#211)

--- tests/fragment.js
+++ tests/fragment.js
@@ -7,7 +7,11 @@ const getOptions = tag => {
         tag,
         context: {},
         index: false,
-        requestFragment: () => {}
+        requestFragment: () => {},
+        defaultAttributes: {
+            'fallback-src': 'https://default-fragment-fallback',
+            custom: 'default-bar'
+        }
     };
 };
 
@@ -37,6 +41,38 @@ describe('Fragment', () => {
 
         const tag = { attributes };
         const fragment = new Fragment(getOptions(tag));
+        const fattributes = fragment.attributes;
+
+        assert.deepEqual(fattributes, expected);
+    });
+
+    it('should fall back to default attribute values if present and value is not set', () => {
+        const attributes = {
+            id: 'foo',
+            src: 'https://fragment',
+            async: true,
+            timeout: '4000'
+        };
+
+        const tag = { attributes };
+        const options = getOptions(tag);
+
+        const { defaultAttributes } = options;
+
+        const expected = {
+            id: attributes.id,
+            url: attributes.src,
+            'fallback-src': defaultAttributes['fallback-src'],
+            src: attributes.src,
+            fallbackUrl: defaultAttributes['fallback-src'],
+            async: attributes.async,
+            timeout: 4000,
+            primary: false,
+            public: false,
+            custom: defaultAttributes.custom
+        };
+
+        const fragment = new Fragment(options);
         const fattributes = fragment.attributes;
 
         assert.deepEqual(fattributes, expected);
