51a673fa61fc535609765c036bb54256f84f8408
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2018-02-07 13:52:42 +0100

    Change hooks to account for multiple script assets
    change node 6 version in travis

--- examples/fragment-performance/index.js
+++ examples/fragment-performance/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const http = require('http');
 const Tailor = require('../../index');
-const serveFragment = require('./fragment');
+const serveFragment = require('../multiple-fragments-with-custom-amd/fragment');
 const tailor = new Tailor({
     templatesPath: __dirname + '/templates',
     pipeAttributes: attributes => {
@@ -10,7 +10,8 @@ const tailor = new Tailor({
             : [];
         const id = attributes.id;
         return { timingGroups, id };
-    }
+    },
+    maxAssetLinks: 3
 });
 const server = http.createServer((req, res) => {
     if (req.url === '/favicon.ico') {
@@ -24,25 +25,19 @@ console.log('Tailor started at port 8080');
 tailor.on('error', (request, err) => console.error(err));
 
 const fragment1 = http.createServer(
-    serveFragment('Header', 'http://localhost:8081')
+    serveFragment('Header', 'http://localhost:8081', 1)
 );
 fragment1.listen(8081);
 console.log('Fragment Header started at port 8081');
 
 const fragment2 = http.createServer(
-    serveFragment('Primary', 'http://localhost:8082')
+    serveFragment('Primary', 'http://localhost:8082', 2)
 );
 fragment2.listen(8082);
 console.log('Fragment Primary started at port 8082');
 
 const fragment3 = http.createServer(
-    serveFragment('Recos', 'http://localhost:8083')
+    serveFragment('Footer', 'http://localhost:8083', 3, true)
 );
 fragment3.listen(8083);
-console.log('Fragment Recos started at port 8083');
-
-const fragment4 = http.createServer(
-    serveFragment('Footer', 'http://localhost:8084')
-);
-fragment4.listen(8084);
-console.log('Fragment Footer started at port 8084');
+console.log('Fragment Footer started at port 8083');
